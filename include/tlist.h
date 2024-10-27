566deb5a9001a39f234c5520ac9e59ad23b46cae
YuliyaPapulina <yuliyapap1510@gmail.com>
2024-10-27 17:47:35 +0300

    added a struc of list

---
+++ include/tlist.h
@@ -0,0 +1,141 @@
+﻿#include <iostream>
+
+template<typename T>
+struct list {
+	struct Node {
+		Node * next;
+		T value;
+		Node(Node * n, T data) {
+			next = n;
+			value = data;
+		}
+	};
+	Node * first = nullptr;
+	list(int n = 0) {
+		if (n < 0) {
+			throw "Not correct the len of the list\n";
+		}
+		while (n) {
+			Node* newNode = new Node(first, 0);
+			first = newNode;
+			n--;
+		}
+	}
+	list(const list & other) {
+		Node* curr1 = other.first;
+		if (curr1 != nullptr) {
+			Node* curr2 = new Node(nullptr, curr1->value);;
+			first = curr2;
+			curr1 = curr1->next;
+			while (curr1) {
+				Node* next1 = new Node(nullptr, curr1->value);
+				curr2->next = next1;
+				curr2 = next1;
+				curr1 = curr1->next;
+			}
+		}
+	}
+	Node & operator[](int index) {
+		int i = 0;
+		Node* curr=first;
+		T result;
+		while (i != index) {
+			curr = curr->next;
+			i++;
+		}
+
+		return *curr;
+	}
+	list& operator=(const list& other) {
+		Node* curr = first;
+		while (first) {
+			curr = first->next;
+			delete first;
+			first = curr;
+		}
+		Node* curr1 = other.first;
+		if (curr1) {
+			Node* curr2 = new Node(nullptr, curr1->value);
+			curr1 = curr1->next;
+			first = curr2;
+			while (curr1) {
+				Node* tmp= new Node(nullptr, curr1->value);
+				curr2->next = tmp;
+				curr2 = curr2->next;
+				curr1 = curr1->next;
+			}
+		}
+		return *this;
+	}
+	list(list&& other) {
+		first = other.first;
+		other.first =nullptr;
+	}
+	size_t size() const {
+		Node* curr = first;
+		size_t count = 0;
+		while (curr) {
+			count++;
+			curr = curr->next;
+		}
+		return count;
+	}
+	void print() {
+		Node* curr = first;
+		while (curr) {
+			std::cout << curr->value << " ";
+			curr = curr->next;
+		}
+		std::cout << std::endl;
+	}
+	Node * insert_front(T data) {
+		first = new Node(first, data);
+		return first;
+	}
+	Node* insert_after(T data, Node * prev) {
+		Node* tmp = new Node(prev->next, data);
+		prev->next = tmp;
+		return tmp;
+	}
+	void erase_afer(Node* prev) {
+		if (prev->next != nullptr) {
+			Node * tmp = prev->next->next;
+			delete prev->next;
+			prev->next = tmp;
+		}
+	}
+	void erase_front() {
+		if (first!= nullptr) {
+			Node * tmp = first->next;
+			delete first;
+			first = tmp;
+		}
+	}
+	void erase_back() {
+		Node * curr = first;
+		while (curr->next->next) {
+			curr = curr->next;
+		}
+		delete curr->next;
+		curr->next = nullptr;
+	}
+	Node * insert_back(T data) {
+		Node * curr = first;
+		while (curr->next) {
+			curr = curr->next;
+		}
+		curr->next = new Node(nullptr, data);
+		return curr;
+	}
+	~list() {
+		Node * curr=first;
+		while (first) {
+			curr = first->next;
+			delete first;
+			first = curr;
+			/*first = curr->next;
+			delete curr;
+			curr = first;*/
+		}
+	}
+};
---
+++ test/test_list.cpp
@@ -0,0 +1,115 @@
+#include "tlist.h"
+
+#include <gtest.h>
+
+TEST(list, can_create_list_positive_len)
+{
+  ASSERT_NO_THROW(list<int> L(5));
+}
+
+TEST(list, can_not_create_list_negitive_len)
+{
+	ASSERT_ANY_THROW(list<int> L(-5));;
+}
+TEST(list, can_create_list_using_copy_construction)
+{
+	list<int> L1(4);
+
+	list<int> L2(L1);
+
+	EXPECT_NE(nullptr, L2.first);
+}
+TEST(list, can_get_size_of_list)
+{
+	size_t size = 10;
+	list<int> L1(size);
+
+	EXPECT_EQ(size, L1.size());
+}
+TEST(list, can_use_moving_constraction)
+{
+	list<int> L1(10);
+	list<int> L2(std::move(L1));
+
+	EXPECT_EQ(L1.first, nullptr);
+}
+TEST(list, can_set_elem)
+{
+	list<int> L1(10);
+	
+	L1[8].value = 100;
+
+	EXPECT_EQ(L1[8].value, 100);
+}
+TEST(list, can_insert_front)
+{
+	list<int> L1(10);
+
+	list<int>::Node* tmp = L1.insert_front(1000);
+
+	EXPECT_EQ(tmp->value, 1000);
+}
+TEST(list, can_insert_after_elem)
+{
+	list<int> L1(10);
+
+	L1.insert_after(1000, &L1[3]);
+
+	EXPECT_EQ(L1[4].value, 1000);
+}
+TEST(list, can_erase_after_elem)
+{
+	list<int> L1(10);
+	L1[6].value = 100;
+	
+	L1.erase_afer(&L1[4]);
+	
+	EXPECT_EQ(L1[5].value, 100);
+}
+TEST(list, can_erase_front_elem)
+{
+	list<int> L1(10);
+	L1[1].value = 100;
+	
+	L1.erase_front();
+	
+	EXPECT_EQ(L1.first->value, 100);
+}
+TEST(list, can_erase_last_elem)
+{
+	list<int> L1(10);
+	L1[9].value = 100;
+
+	L1.erase_back();
+	
+	EXPECT_EQ(L1[8].next, nullptr);
+}
+TEST(list, can_insert_at_the_end_of_list)
+{
+	list<int> L1(10);
+	
+	L1.insert_back(100);
+	
+	EXPECT_EQ(L1[10].value, 100);
+}
+TEST(list, can_assign_other_list_equal_size)
+{
+	size_t size1 = 10, size2 = 10;
+	list<int> L1(size1),L2(size2);
+	L1[5].value = 10;
+	
+	L2 = L1;
+	
+	EXPECT_EQ(L2[5].value,10);
+}
+TEST(list, can_assign_other_list_not_equal_size)
+{
+	size_t size1 = 10, size2 = 12;
+	list<int> L1(size1), L2(size2);
+	L2[10].value = 10;
+
+	L1 = L2;
+	
+	EXPECT_EQ(((L1.size()==size2)&&(L1[10].value==10)), 1);
+}
+
